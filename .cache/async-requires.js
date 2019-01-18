a54c4fbb6e3416811688c4781e4768b2ed91a794
刘光愚 <lgy@sc.10086.com>
2019-01-18 21:12:30 +0800

    更新

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---src-templates-posts-js": () => import("/Users/lgy/gatsby_test/blog_gatsby/src/templates/posts.js" /* webpackChunkName: "component---src-templates-posts-js" */),
   "component---src-templates-blog-post-js": () => import("/Users/lgy/gatsby_test/blog_gatsby/src/templates/blog-post.js" /* webpackChunkName: "component---src-templates-blog-post-js" */),
-  "component---cache-dev-404-page-js": () => import("/Users/lgy/gatsby_test/blog_gatsby/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-about-js": () => import("/Users/lgy/gatsby_test/blog_gatsby/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-my-files-js": () => import("/Users/lgy/gatsby_test/blog_gatsby/src/pages/my-files.js" /* webpackChunkName: "component---src-pages-my-files-js" */)
 }
