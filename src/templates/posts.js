3c75e15214e643c74c88c0d2e79653a39a58d50e
97thjingba <1079937489@qq.com>
2019-06-23 22:19:18 +0800

    rewrite/post-card

--- src/templates/posts.js
+++ src/templates/posts.js
@@ -1,12 +1,28 @@
-import React from "react"
-import Layout from "../components/layout"
-import { Link, graphql, } from "gatsby"
-import { Card, Section, SubTitle, Media, Content, Title } from 'reactbulma'
-import './posts.css'
+import React from "react";
+import Layout from "../components/layout";
+import { Link, graphql } from "gatsby";
+import { Section, SubTitle, Media, Content, Title } from "reactbulma";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import "./posts.css";
+import useStyles from "../style/cardStyle"
 
 const NavLink = props => {
     if (!props.test) {
-        return <Link to={props.url} id="navlink" className="has-text-primary">{props.text}</Link>;
+        return (
+            <Link to={props.url} id="navlink" className="has-text-primary">
+                {props.text}
+            </Link>
+        );
     } else {
         return <span>{props.text}</span>;
     }
@@ -15,29 +31,29 @@ const NavLink = props => {
 export const query = graphql`
   query {
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-        totalCount  
-        edges {
-          node {
-            id
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
-            excerpt(pruneLength:50)
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date
           }
+          fields {
+            slug
+          }
+          excerpt(pruneLength: 50)
         }
       }
+    }
   }
-`
+`;
 
 const IndexPage = ({ data, pathContext }) => {
     const { group, index, first, last, pageCount } = pathContext;
     const previousUrl = index - 1 == 1 ? "" : (index - 1).toString();
     const nextUrl = (index + 1).toString();
-
+    const classes = useStyles();
 
     return (
         <div>
@@ -49,33 +65,60 @@ const IndexPage = ({ data, pathContext }) => {
                             Record every day of life
                         </SubTitle>
                         <div>
-                            <h4 className="center">{data.allMarkdownRemark.totalCount}篇小文章</h4>
+                            <h4 className="center">
+                                {data.allMarkdownRemark.totalCount}篇小文章
+                            </h4>
                             <div className="columns is-multiline">
                                 {group.map(({ node }) => (
                                     <div className="column is-one-third">
                                         <div key={node.id} className="blog_style">
-                                            <Link to={node.fields.slug}>
-                                                <Card className="card_style">
-                                                    <Card.Image src={node.frontmatter.image.childImageSharp.fluid.src} ratio='5by3' />
-                                                    <Card.Content>
-                                                        <Media>
-                                                            <Media.Content>
-                                                                <Title is='4'>
-                                                                    test_title
-                                                                </Title>
-                                                               
-                                                            </Media.Content>
-                                                        </Media>
-                                                        <Content>
-                                                            <p className="title is-6">
-                                                                {node.frontmatter.coverText}
-                                                            </p>
-                                                            <br></br>
-                                                            <small>{node.frontmatter.date}</small>
-                                                        </Content>
-                                                    </Card.Content>
-                                                </Card>
-                                            </Link>
+
+                                            <Card className={classes.card}>
+                                                <Link to={node.fields.slug}>
+                                                    <CardHeader
+                                                        avatar={
+                                                            <Avatar
+                                                                aria-label="Recipe"
+                                                                className={classes.avatar}
+                                                            >
+                                                                R
+                                                            </Avatar>
+                                                        }
+                                                        action={
+                                                            <IconButton aria-label="Settings">
+                                                                <MoreVertIcon />
+                                                            </IconButton>
+                                                        }
+                                                        title="Shrimp and Chorizo Paella"
+                                                        subheader="September 14, 2016"
+                                                    />
+                                                    <CardMedia
+                                                        className={classes.media}
+                                                        image={node.frontmatter.image.childImageSharp.fluid.src}
+                                                        title="Paella dish"
+                                                    />
+                                                    <CardContent>
+                                                        <Typography
+                                                            variant="body2"
+                                                            color="textSecondary"
+                                                            component="p"
+                                                        >
+                                                            This impressive paella is a perfect party dish and
+                                                            a fun meal to cook together with your guests. Add
+                                                            1 cup of frozen peas along with the mussels, if
+                                                            you like.
+                                                    </Typography>
+                                                    </CardContent>
+                                                    <CardActions disableSpacing>
+                                                        <IconButton aria-label="Add to favorites">
+                                                            <FavoriteIcon />
+                                                        </IconButton>
+                                                        <IconButton aria-label="Share">
+                                                            <ShareIcon />
+                                                        </IconButton>
+                                                    </CardActions>
+                                                </Link>
+                                            </Card>
                                         </div>
                                     </div>
                                 ))}
