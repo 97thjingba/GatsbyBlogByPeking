4787215eb26ef4a6fb7ac8a16b98cca444bbbf5a
97thjingba <1079937489@qq.com>
2019-06-27 15:25:04 +0800

    add_icon_by_reacr-icons

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import { StaticQuery, graphql } from "gatsby"
 import './layout.css'
 import { Helmet } from 'react-helmet'
 import { SectionsContainer, Section } from 'react-fullpage-lgy';
+import { FaGithub, FaTencentWeibo } from 'react-icons/fa';
 
 let options = {
     sectionClassName: 'section',
@@ -46,8 +47,9 @@ export default ({ children }) => (
                     <footer class="footer">
                         <div class="content has-text-centered">
                             <p>
-                                <p className="subtitle has-text-black is-6">永远年轻,永远热泪盈眶.   —— Jack Kerouac 《达摩流浪者》</p>
+                                <p className="subtitle has-text-black is-7">永远年轻,永远热泪盈眶.   —— Jack Kerouac 《达摩流浪者》</p>
                                 <p className="has-text-primary subtitle is-6">Build with gatsby -- by Peking</p>
+                                <a href="https://github.com/97thjingba/blog_gatsby"><FaGithub size="1.5rem" /></a>
                             </p>
                         </div>
                     </footer>
