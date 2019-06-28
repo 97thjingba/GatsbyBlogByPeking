53ed29f952dc24f682a5549b7d594a0f86a18d0b
97thjingba <1079937489@qq.com>
2019-06-28 22:07:07 +0800

    update

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,26 +3,17 @@ import { graphql } from "gatsby";
 import { Container } from "reactbulma";
 import "./blog-post.css";
 import Footer from "../components/Footer/footer";
-import FullHeader from 'react-full-header';
 
 export default ({ data }) => {
     const post = data.markdownRemark
     return (
         <div>
-            {/* <FullHeader
-                title="Peking"
-                subtitle="lgy's blog -- by gatsby."
-                bgColor="#EBE9EB"
-                textColor="#3299BB"
-                font="Lobster"
-                bgImg={post.frontmatter.image.publicURL}
-            /> */}
             <div className="body_background">
                 <div className="container has-background-white blog_post_container">
                     <div className="hero-body font-size font-weight">
                         <div dangerouslySetInnerHTML={{ __html: post.html }}/>
                     </div> 
-                </div>
+                </div>  
             </div>
             <Footer />
         </div>
