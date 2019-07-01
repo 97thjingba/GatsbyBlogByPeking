931c7117505a440b5e100a11d8f50506310c7fa6
97thjingba <1079937489@qq.com>
2019-07-01 15:26:32 +0800

    give up react-lazy-img

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,21 +3,12 @@ import { graphql } from "gatsby";
 import { Container } from "reactbulma";
 import "./blog-post.css";
 import Footer from "../components/Footer/Footer";
-import Img from "react-lazy-img";
+
 
 export default ({ data }) => {
     const post = data.markdownRemark
     return (
         <div>
-            <div>
-                <Img
-                    src={post.frontmatter.image.publicURL}
-                    width={2000}
-                    height={400}
-                    offset={1}
-                    transition="opacity 1s ease-in"
-                />
-            </div>
             <div className="has-background-black">
                 <div className="container has-background-white blog_post_container blog_post_box">
                     <div className="hero-body font-size font-weight">
