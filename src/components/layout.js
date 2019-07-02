51a3b6c9d2723953ded9224820bca017621d45ef
97thjingba <1079937489@qq.com>
2019-07-02 11:53:36 +0800

    update/no

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { Component } from "react"
 import { StaticQuery, graphql } from "gatsby"
-import './layout.css'
 import { Helmet } from 'react-helmet'
 import { SectionsContainer, Section } from 'react-fullpage-lgy';
 import { FaGithub, FaTencentWeibo } from 'react-icons/fa';
-import Footer from "./Footer/Footer"
+import Footer from "./Footer/Footer";
+import DrawerLeft from "./Drawer/DrawerLeft"
+import './layout.css'
 
 let options = {
     sectionClassName: 'section',
@@ -14,40 +15,55 @@ let options = {
     navigation: false,
 };
 
+class layout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-export default ({ children }) => (
-    <StaticQuery
-        query={graphql`
-            query {
-                site {
-                siteMetadata {
-                    title,
-                    description,
+    _Wor = () => {
+        return (
+        <StaticQuery
+            query={graphql`
+                query {
+                    site {
+                    siteMetadata {
+                        title,
+                        description,
+                    }
+                    }
                 }
-                }
+                `
             }
-            `
-        }
 
-        render={data => (
-            <body>
-                <Helmet>
-                    <html lang="en" />
-                    <title>{data.site.siteMetadata.title}</title>
-                    <meta name="description" content={data.site.siteMetadata.description} />
-                </Helmet>
-                <div>
-                    <SectionsContainer {...options}>
-                        <Section id="slide">
-                            <div className="center">
-                                <h1 className="has-text-white title title_position">LGY'S blog - by GATSBY</h1>
-                            </div>
-                        </Section>
-                    </SectionsContainer>
-                    {children}
-                    <Footer/>
-                </div>
-            </body>
-        )}
-    />
-)
+            render={ data => (
+                <body>
+                    <Helmet>
+                        <html lang="en" />
+                        <title>{data.site.siteMetadata.title}</title>
+                        <meta name="description" content={data.site.siteMetadata.description} />
+                    </Helmet>
+                    <div>
+
+                        <SectionsContainer {...options}>
+                            <Section id="slide">
+                                <div className="center">
+                                    <h1 className="has-text-white title title_position">LGY'S blog - by GATSBY</h1>
+                                </div>
+                            </Section>
+                        </SectionsContainer>
+                    </div>
+                </body>
+            ) }
+        />)
+
+    }
+
+    render() {
+        return (
+            <div>{this._Wor()}</div>
+        );
+    }
+}
+
+export default layout;
