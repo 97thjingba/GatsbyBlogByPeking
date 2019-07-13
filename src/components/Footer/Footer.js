4c8c334654ae83889901c81064bb75d32222875a
97thjingba <1079937489@qq.com>
2019-07-13 21:09:07 +0800

    change_footer_a_github_color

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,9 @@ import styled from "styled-components";
 const FooterBox = styled.section`
     max-height:150px
 `
+const GithubLink = styled.a`
+    color:black
+`
 class Footer extends Component {
     constructor(props) {
         super(props);
@@ -20,7 +23,7 @@ class Footer extends Component {
                         <p>
                             <p className="subtitle has-text-black is-7">永远年轻,永远热泪盈眶.   —— Jack Kerouac 《达摩流浪者》</p>
                             <p className="has-text-primary subtitle is-6">Build with gatsby -- by Peking</p>
-                            <a href="https://github.com/97thjingba/blog_gatsby"><FaGithub size="1.5rem" /></a>
+                            <GithubLink href="https://github.com/97thjingba/blog_gatsby"><FaGithub size="1.5rem" /></GithubLink>
                         </p>
                     </div>
                 </footer>
